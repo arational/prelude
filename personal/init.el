32ce7d1ce9f9e02b8de39ca0e01bbc6989761c38
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2016-09-30 13:54:52 +0200

    remove obsolete options

--- personal/init.el
+++ personal/init.el
@@ -1,5 +1,1 @@
-(with-eval-after-load 'smartparens
-  (define-key smartparens-mode-map (kbd "M-[") 'sp-backward-unwrap-sexp)
-  (define-key smartparens-mode-map (kbd "M-]") 'sp-unwrap-sexp))
-
 (scroll-bar-mode -1)
