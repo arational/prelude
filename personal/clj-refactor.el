20f2eb3aba22997e51c8a9c08c835ef44d33d7b2
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2016-10-24 11:55:00 +0200

    remave yas-snippet

--- personal/clj-refactor.el
+++ personal/clj-refactor.el
@@ -2,7 +2,6 @@
 
 (defun my-clojure-mode-hook ()
   (clj-refactor-mode 1)
-  (yas-minor-mode 1) ; for adding require/use/import statements
   ;; This choice of keybinding leaves cider-macroexpand-1 unbound
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
