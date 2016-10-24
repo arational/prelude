91952e37b356b52c16207785645de79dff699a81
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2016-10-24 11:56:19 +0200

    add code to reset unused evil-bindings

--- personal/evil.el
+++ personal/evil.el
@@ -13,3 +13,9 @@
                                  (cider-stacktrace-mode . emacs)
                                  (cider-test-report-mode . emacs))
          do (evil-set-initial-state mode state))
+
+(with-eval-after-load 'evil-maps
+  (define-key evil-normal-state-map (kbd "C-.") nil)
+  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (define-key evil-motion-state-map (kbd "RET") nil)
+  (define-key evil-motion-state-map (kbd "TAB") nil))
