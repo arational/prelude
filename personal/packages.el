7df65c814794d20194a80b50a97540b7ac744834
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2016-10-24 11:55:53 +0200

    remove package evil-magit

--- personal/packages.el
+++ personal/packages.el
@@ -1,5 +1,4 @@
 (prelude-require-packages '(ag
                             clj-refactor
-                            evil-magit
                             relative-line-numbers
                             evil-smartparens))
