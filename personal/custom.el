7084ab8cf1edb9e3e4b9dd5efdb7db3ee572eff8
Ivan Stefanischin <ivan@stefanischin.eu>
2016-12-25 17:19:35 +0100

    change font size

--- personal/custom.el
+++ personal/custom.el
@@ -68,4 +68,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "1ASC" :family "Liberation Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 109 :width normal :foundry "1ASC" :family "Liberation Mono")))))
