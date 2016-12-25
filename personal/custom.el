af59ac08117db9400310d55981950fa2e2713ed2
Ivan Stefanischin <ivan@stefanischin.eu>
2016-12-25 14:07:28 +0100

    add custom.el

---
+++ personal/custom.el
@@ -0,0 +1,71 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" default)))
+ '(ee-export-file "~/var/effort.csv")
+ '(ee-subtree-tag "redmine")
+ '(ee-user "liquid")
+ '(electric-indent-mode t)
+ '(enable-local-variables nil)
+ '(fci-rule-color "#383838")
+ '(geiser-default-implementation (quote chicken))
+ '(geiser-mode-start-repl-p t t)
+ '(global-linum-mode nil)
+ '(global-relative-line-numbers-mode nil)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(linum-format (quote linum-relative))
+ '(mu4e-change-filenames-when-moving t)
+ '(mu4e-maildir "/home/ivan/.mail")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-clock-continuously t)
+ '(org-ellipsis "[...]")
+ '(org-replace-disputed-keys t)
+ '(org-return-follows-link t)
+ '(org-src-fontify-natively t)
+ '(org-support-shift-select nil)
+ '(package-selected-packages
+   (quote
+    (mu4e evil-mu4e evil-smartparens relative-line-numbers org-evil zop-to-char zenburn-theme yaml-mode which-key volatile-highlights smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters pkgbuild-mode ov operate-on-number move-text markdown-mode magit key-chord json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region evil-visualstar evil-surround evil-numbers elisp-slime-nav effort-export easy-kill discover-my-major diminish diff-hl csv-mode crux company clj-refactor browse-kill-ring beacon anzu ag ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(scheme-program-name "csi")
+ '(send-mail-function (quote sendmail-send-it))
+ '(sendmail-program "msmtp")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
