7d7fa21f648608d0583d78c993ad42cc6403fd0f
Ivan Stefanischin <ivan@stefanischin.eu>
2017-03-29 12:40:02 +0200

    remove spell checking config

--- personal/hunspell.el
+++
@@ -1,5 +0,0 @@
-(setq ispell-program-name "hunspell")
-(setq ispell-local-dictionary "de_DE")
-(setq ispell-local-dictionary-alist
-      '(("de_DE" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8)))
-(flyspell-mode 1)
