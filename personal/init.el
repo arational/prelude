98c0f95666e13d27e7a80cd85cd0fa17737e9146
Ivan Stefanischin <ivan@stefanischin.eu>
2017-04-28 10:51:44 +0200

    add helper function to insert randomly generated uuid

--- personal/init.el
+++ personal/init.el
@@ -1,1 +1,7 @@
 (scroll-bar-mode -1)
+
+(defun insert-uuid ()
+  "Generates a UUID via `uuidgen` and inserts it into the current buffer."
+  (interactive)
+  (call-process "uuidgen" nil t)
+  (delete-backward-char 1))
