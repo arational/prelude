5b2cf13851f65e5522f19720c07782ff32d148d9
Ivan Stefanischin <ivan@stefanischin.eu>
2017-06-02 16:40:23 +0200

    add helper function to prepend the ticket number

--- personal/init.el
+++ personal/init.el
@@ -9,3 +9,15 @@
 (add-to-list 'auto-mode-alist
              '("\\.cl\\'" . (lambda ()
                                (c-mode))))
+
+;; Adds the ticket number into the commit message
+(defun my-git-commit-setup ()
+  (when (string-match-p "^/home/ivan/src/bevuta" (magit-git-dir))
+    (let ((branch (magit-get-current-branch)))
+      (when (string-match "-\\([[:digit:]]\\{3,\\}\\)" branch)
+        (let ((prefix (format "[%s] \n" (match-string 1 branch))))
+          (goto-char (point-min))
+          (insert prefix)
+          (forward-char -1))))))
+
+(add-hook 'git-commit-mode-hook 'my-git-commit-setup)
