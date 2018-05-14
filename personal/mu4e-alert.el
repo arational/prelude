87ad0d6485a44c08fe524648a0d2bd289ee7a7ec
Ivan Stefanischin <ivan@stefanischin.eu>
2018-05-14 15:12:22 +0200

    add functionality for automatic mail synchronization and notification

---
+++ personal/mu4e-alert.el
@@ -0,0 +1,10 @@
+;; Choose the style you prefer for desktop notifications
+;; If you are on Linux you can use
+;; 1. notifications - Emacs lisp implementation of the Desktop Notifications API
+;; 2. libnotify     - Notifications using the `notify-send' program, requires `notify-send' to be in PATH
+;;
+;; On Mac OSX you can set style to
+;; 1. notifier      - Notifications using the `terminal-notifier' program, requires `terminal-notifier' to be in PATH
+;; 1. growl         - Notifications using the `growl' program, requires `growlnotify' to be in PATH
+(mu4e-alert-set-default-style 'libnotify)
+(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
