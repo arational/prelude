61cdec41ee821d3ea54f35cc637eb7786b250166
Ivan Stefanischin <ivan@stefanischin.eu>
2018-05-14 15:10:46 +0200

    let alerts stay a bit longer

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(alert-fade-time 20)
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
