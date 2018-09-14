e52eaaba01c05713a087bea0a7d7e7ad99ff7c1e
Ivan Stefanischin <ivan@stefanischin.eu>
2018-09-14 15:38:05 +0200

    reconfigure modules because of prelude-update

--- prelude-modules.el
+++ prelude-modules.el
@@ -2,19 +2,19 @@
 
 ;; Emacs IRC client
 (require 'prelude-erc)
-(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-(require 'prelude-helm) ;; Interface for narrowing and search
-(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-ivy) ;; A mighty modern alternative to ido
+;; (require 'prelude-helm) ;; Interface for narrowing and search
+;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
-;; (require 'prelude-mediawiki)
 ;; (require 'prelude-evil)
 
 ;;; Programming languages support
 (require 'prelude-c)
-(require 'prelude-clojure)
+;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
-(require 'prelude-common-lisp)
+;; (require 'prelude-common-lisp)
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
@@ -29,10 +29,11 @@
 (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
+;; (require 'prelude-rust)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-(require 'prelude-yaml)
+;; (require 'prelude-yaml)
