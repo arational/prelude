f308f4f3e239dd79ffd45b66a51f0e4613662a7b
Ivan Stefanischin <ivan@stefanischin.eu>
2019-10-08 11:40:27 +0200

    add melpa-stable

--- personal/packages.el
+++ personal/packages.el
@@ -1,3 +1,6 @@
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
 (prelude-require-packages '(ag
                             clj-refactor
                             rcirc-notify
