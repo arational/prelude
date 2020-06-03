22349700ad65595e5130fd2182a49fa567b8996b
Ivan Stefanischin <ivan@stefanischin.eu>
2020-06-03 17:12:40 +0200

    define shortcuts for google translator

---
+++ personal/google-translate.el
@@ -0,0 +1,5 @@
+(require 'google-translate)
+(require 'google-translate-default-ui)
+
+(global-set-key (kbd "C-c M-t p") 'google-translate-at-point)
+(global-set-key (kbd "C-c M-t q") 'google-translate-query-translate)
