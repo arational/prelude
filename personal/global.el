16319ba6a4cb54e81dd69ad487b179779c2a1d9a
Ivan Stefanischin <ivan@stefanischin.eu>
2020-06-03 17:11:09 +0200

    add some useful scrolling shortcuts

--- personal/global.el
+++ personal/global.el
@@ -7,3 +7,9 @@
 
 ;; Wrap in square brackets
 (global-set-key (kbd "M-[") 'paredit-wrap-square)
+
+;; keep cursor at same position when scrolling and add keywbindings to
+;; scroll the screen when moving the cursor
+(setq scroll-preserve-screen-position 1)
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
