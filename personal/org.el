447cdd1df5e405d72454e869addb5a0d146189dd
Ivan Stefanischin <ivan@stefanischin.eu>
2020-12-09 16:19:10 +0100

    add ispell behaviour for org-mode

--- personal/org.el
+++ personal/org.el
@@ -5,3 +5,12 @@
   (define-key org-read-date-minibuffer-local-map (kbd "C-n")
     (lambda () (interactive)
       (org-eval-in-calendar '(calendar-forward-day 1)))))
+
+(defun org-ispell ()
+  "Configure `ispell-skip-region-alist' for `org-mode'."
+  (make-local-variable 'ispell-skip-region-alist)
+  (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
+  (add-to-list 'ispell-skip-region-alist '("~" "~"))
+  (add-to-list 'ispell-skip-region-alist '("=" "="))
+  (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC" . "^#\\+END_SRC")))
+(add-hook 'org-mode-hook #'org-ispell)
